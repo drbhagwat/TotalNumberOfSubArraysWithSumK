47d83dc97e1fe7165ce729af7f6e5aa1522ec30e
Dinesh Bhagwat <dinesh.bhagwat@gmail.com>
2026-07-27 13:58:14 +0530

    Restructured the code.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,61 +1,52 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     private int[] data;
-    private int numberOfElements;
     private int requiredSum;
 
     public static void main() {
         Main main = new Main();
         main.read();
         main.print();
-        System.out.println(main.subarraySum(main.getData(), main.getRequiredSum()));
-    }
-
-    private int[] getData() {
-        return data;
+        int requiredSum = main.getRequiredSum();
+        System.out.println("# of subarrays whose sum is " + requiredSum + " is " + main.subarraySum(requiredSum));
     }
 
     private int getRequiredSum() {
         return requiredSum;
     }
 
-    public Main() {
-    }
-
     public void read() {
         System.out.print("Enter the number of elements to be stored in an array : ");
         Scanner scanner = new Scanner(System.in);
-        numberOfElements = scanner.nextInt();
+        int numberOfElements = scanner.nextInt();
         data = new int[numberOfElements];
-
         System.out.print("Now, read the elements one by one (each separated by a whitespace (space, tab, or newline)): ");
 
         for (int i = 0; i < data.length; i++) {
             data[i] = scanner.nextInt();
         }
-        System.out.print("Now Enter the value os k so, I can find out how many subarrays add up to K ");
+        System.out.print("What is the required Sum? ");
         requiredSum = scanner.nextInt();
         scanner.close();
     }
 
     public void print() {
-        for (int i = 0; i < numberOfElements; i++) {
-            System.out.println(data[i] + "\t");
-        }
-        System.out.println("\n");
+        System.out.print("The array is as follows: ");
+        System.out.println(Arrays.toString(data));
     }
 
-    public int subarraySum(int[] nums, int k) {
+    public int subarraySum(int k) {
         int count = 0;
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < data.length; i++) {
             int sum = 0;
 
-            for(int j = i; j < nums.length; j++) {
-                sum += nums[j];
+            for(int j = i; j < data.length; j++) {
+                sum += data[j];
 
                 if (sum == k) {
                     count++;
